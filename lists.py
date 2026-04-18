f5a8d3c80d422bf7c2854b94b10a6faff4a01934
moiseevadarya08 <dashamoiseeva08@mail.ru>
2026-04-18 23:41:58 +0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -2,8 +2,17 @@
 numbers = [3, 7, 2, 9]
 for i in numbers:
     print(i) 
+
 # Задача 2 
 numbers = [3, 7, 2, 9, 4 ,6]
 for i in numbers:
     if i % 2 == 0:
         print(i)
+
+# Задача 3
+count = 0
+numbers = [5, 8, 3, 10, 2, 7]
+for i in numbers:
+    if i % 2 == 0:
+        count += 1
+print(count)
