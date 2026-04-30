085c35b5476a37ccc17bceed41a15c36e53ca2eb
moiseevadarya08 <dashamoiseeva08@mail.ru>
2026-04-30 22:11:32 +0300

    Create 03_loops.py

---
+++ 03_loops.py
@@ -0,0 +1,17 @@
+# Задача 1: Вывести числа из списка
+numbers = [1, 2, 3, 4]
+for i in numbers:
+    print(i)
+
+# Задача 2: Посчитать сумму чисел
+numbers = [1, 2, 3, 4]
+total = 0
+for i in numbers:
+    total += i
+print(total)
+
+# Задача 3: Найти чётные числа
+numbers = [2, 5, 8, 11]
+for i in numbers:
+    if i % 2 == 0:
+        print(i)
