138e1f3bfa6a73a2f517a4946bdd3fe9752c4871
moiseevadarya08 <dashamoiseeva08@mail.ru>
2026-05-01 20:36:48 +0300

    Create 13_mini_calculator.py

---
+++ 13_mini_calculator.py
@@ -0,0 +1,11 @@
+num1 = int(input("Введите число 1:"))
+num2 = int(input("Введите число 2:"))
+operation = input("Введите операцию (+, -, *, /)")
+if operation == "+":
+    print(num1 + num2)
+if operation == "-":
+    print(num2 - num1)
+if operation == "*":
+    print(num1 * num2)
+if operation == "/":
+    print(num1 / num2)
